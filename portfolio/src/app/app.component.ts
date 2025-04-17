40533c54f9975bae6c8bd79730c89fb26c3b5b64
jp <ssupawej@gmail.com>
2025-04-17 13:37:57 +0700

    some html

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'portfolio'
+  year = new Date().getFullYear();
 }
